bc58f866b943c5da1937733e95cfca1f6828d806
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-06 14:57:04 -0300

    Changing for deploy

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
-
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/client'));
-app.get('/*', function(req, res) {
+
+app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
 
-var listener = server.listen(process.env.PORT || 5000, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
