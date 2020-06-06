2a08b45c576468282f4e39158c269cbb5fbffa4b
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-06 14:08:53 -0300

    Ajuste package.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
 
-app.listen(5000);
-
+var listener = server.listen(process.env.PORT || 5000, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+});
